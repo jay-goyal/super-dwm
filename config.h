9f2a8d9e20e383ec3336ba751faa9b653d6cc6df
Jay Goyal <goyal.jay2003@gmail.com>
2022-01-23 13:07:44 +0530

    Made asus profile switching much better

--- config.h
+++ config.h
@@ -36,19 +36,18 @@ static const Rule rules[] = {
      *	WM_CLASS(STRING) = instance, class
      *	WM_NAME(STRING) = title
      */
-    /* class             instance    title       tags mask     isfloating
-       monitor */
-    {"firefox", NULL, NULL, 1, 0, -1},
-    {"firefoxdeveloperedition", NULL, NULL, 1, 0, -1},
-    {"Brave-browser", NULL, NULL, 1, 0, -1},
-    {"Code", NULL, NULL, 1 << 1, 0, -1},
-    {"jetbrains-clion", NULL, NULL, 1 << 1, 0, -1},
-    {"jetbrains-webstorm", NULL, NULL, 1 << 1, 0, -1},
-    {"Virt-manager", NULL, NULL, 1 << 2, 0, -1},
-    {"Spotify", NULL, NULL, 1 << 3, 0, -1},
-    {"vlc", NULL, NULL, 1 << 4, 0, -1},
-    {"discord", NULL, NULL, 1 << 5, 0, -1},
-    {"qBittorrent", NULL, NULL, 1 << 6, 0, -1},
+    /* class                        instance    title       tags mask     isfloating       monitor */
+    {"firefox",                         NULL, NULL,      1,      0, -1},
+    {"firefoxdeveloperedition",         NULL, NULL,      1,      0, -1},
+    {"Brave-browser",                   NULL, NULL,      1,      0, -1},
+    {"Code",                            NULL, NULL,      1 << 1, 0, -1},
+    {"jetbrains-clion",                 NULL, NULL,      1 << 1, 0, -1},
+    {"jetbrains-webstorm",              NULL, NULL,      1 << 1, 0, -1},
+    {"Virt-manager",                    NULL, NULL,      1 << 2, 0, -1},
+    {"Spotify",                         NULL, "Spotify", 1 << 3, 0, -1},
+    {"vlc",                             NULL, NULL,      1 << 4, 0, -1},
+    {"discord",                         NULL, NULL,      1 << 5, 0, -1},
+    {"qBittorrent",                     NULL, NULL,      1 << 6, 0, -1},
 };
 
 /* layout(s) */
@@ -105,61 +104,59 @@ static const char *mic_mute[] = {"pactl", "set-source-mute", "@DEFAULT_SOURCE@",
                                 };
 static const char *brightness_up[] = {"xbacklight", "-inc", "5.0", NULL};
 static const char *brightness_down[] = {"xbacklight", "-dec", "5.0", NULL};
-static const char *profile_set[] = {"asusctl", "profile", "-n", NULL};
+static const char *profile_set[] = {"/home/jay/.local/bin/asus_profile.sh", NULL};
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
     /* PROGRAMS */
-    {   MODKEY, XK_r, spawn,
+    {MODKEY, XK_r, spawn,
         SHCMD("j4-dmenu-desktop --dmenu=\"dmenu -h 27 -l 20 -bw 1 -p 'Run: '\" "
               "--term=\"alacritty\"")
     },
-    {MODKEY, XK_x, spawn, {.v = termcmd}},
-    {MODKEY, XK_b, spawn, {.v = browser}},
-    {MODKEY, XK_c, spawn, {.v = editor}},
-    {MODKEY, XK_e, spawn, {.v = file}},
-    {MODKEY, XK_p, spawn, {.v = pdf}},
-    {MODKEY, XK_q, killclient, {0}},
+    {MODKEY,              XK_x,              spawn,         {.v = termcmd}},
+    {MODKEY,              XK_b,              spawn,         {.v = browser}},
+    {MODKEY,              XK_c,              spawn,         {.v = editor}},
+    {MODKEY,              XK_e,              spawn,         {.v = file}},
+    {MODKEY,              XK_p,              spawn,         {.v = pdf}},
+    {MODKEY,              XK_q,              killclient,    {0}},
     /* LAYOUT */
-    {MODKEY | ShiftMask, XK_j, rotatestack, {.i = +1}},
-    {MODKEY | ShiftMask, XK_k, rotatestack, {.i = -1}},
-    {MODKEY, XK_i, incnmaster, {.i = +1}},
-    {MODKEY, XK_d, incnmaster, {.i = -1}},
-    {MODKEY, XK_j, focusstack, {.i = +1}},
-    {MODKEY, XK_k, focusstack, {.i = -1}},
-    {MODKEY | ShiftMask, XK_h, setmfact, {.f = -0.05}},
-    {MODKEY | ShiftMask, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY, XK_Tab, cyclelayout, {.i = +1}},
-    {MODKEY | ShiftMask, XK_f, togglefloating, {0}},
-    {MODKEY, XK_f, togglefullscr, {0}},
-    {MODKEY, XK_0, view, {.ui = ~0}},
-    {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
-    {MODKEY, XK_comma, focusmon, {.i = -1}},
-    {MODKEY, XK_period, focusmon, {.i = +1}},
-    {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
-    {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
-    {MODKEY, XK_minus, setgaps, {.i = -1}},
-    {MODKEY, XK_equal, setgaps, {.i = +1}},
-    {MODKEY | ShiftMask, XK_equal, setgaps, {.i = 0}},
+    {MODKEY | ShiftMask,  XK_j,              rotatestack,   {.i = +1}},
+    {MODKEY | ShiftMask,  XK_k,              rotatestack,   {.i = -1}},
+    {MODKEY,              XK_i,              incnmaster,    {.i = +1}},
+    {MODKEY,              XK_d,              incnmaster,    {.i = -1}},
+    {MODKEY,              XK_j,              focusstack,    {.i = +1}},
+    {MODKEY,              XK_k,              focusstack,    {.i = -1}},
+    {MODKEY | ShiftMask,  XK_h,              setmfact,      {.f = -0.05}},
+    {MODKEY | ShiftMask,  XK_l,              setmfact,      {.f = +0.05}},
+    {MODKEY,              XK_Tab,            cyclelayout,   {.i = +1}},
+    {MODKEY | ShiftMask,  XK_f,              togglefloating,{0}},
+    {MODKEY,              XK_f,              togglefullscr, {0}},
+    {MODKEY,              XK_0,              view,          {.ui = ~0}},
+    {MODKEY | ShiftMask,  XK_0,              tag,           {.ui = ~0}},
+    {MODKEY,              XK_comma,          focusmon,      {.i = -1}},
+    {MODKEY,              XK_period,         focusmon,      {.i = +1}},
+    {MODKEY | ShiftMask,  XK_comma,          tagmon,        {.i = -1}},
+    {MODKEY | ShiftMask,  XK_period,         tagmon,        {.i = +1}},
+    {MODKEY,              XK_minus,          setgaps,       {.i = -1}},
+    {MODKEY,              XK_equal,          setgaps,       {.i = +1}},
+    {MODKEY | ShiftMask,  XK_equal,          setgaps,       {.i = 0}},
     /* TAGS */
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
     TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
     TAGKEYS(XK_9, 8)
     /* SYSTEM */
-    {
-        MODKEY | ShiftMask, XK_q, quit, {0}
-    },
-    {MODKEY | ShiftMask, XK_r, quit, {1}},
-    {MODKEY | ControlMask, XK_s, spawn, SHCMD("shutdown now")},
-    {MODKEY | ControlMask, XK_r, spawn, SHCMD("reboot")},
+    {MODKEY | ShiftMask,   XK_q,             quit,          {0}},
+    {MODKEY | ShiftMask,   XK_r,             quit,          {1}},
+    {MODKEY | ControlMask, XK_s,             spawn,         SHCMD("shutdown now")},
+    {MODKEY | ControlMask, XK_r,             spawn,         SHCMD("reboot")},
     /* VOULUME */
-    {0, XF86XK_AudioLowerVolume, spawn, {.v = vol_up}},
-    {0, XF86XK_AudioRaiseVolume, spawn, {.v = vol_down}},
-    {0, XF86XK_AudioMute, spawn, {.v = mute}},
-    {0, XF86XK_AudioMicMute, spawn, {.v = mic_mute}},
-    {0, XF86XK_MonBrightnessUp, spawn, {.v = brightness_up}},
-    {0, XF86XK_MonBrightnessDown, spawn, {.v = brightness_down}},
-    {0, XF86XK_Launch4, spawn, {.v = profile_set}},
+    {0, XF86XK_AudioLowerVolume,             spawn,         {.v = vol_up}},
+    {0, XF86XK_AudioRaiseVolume,             spawn,         {.v = vol_down}},
+    {0, XF86XK_AudioMute,                    spawn,         {.v = mute}},
+    {0, XF86XK_AudioMicMute,                 spawn,         {.v = mic_mute}},
+    {0, XF86XK_MonBrightnessUp,              spawn,         {.v = brightness_up}},
+    {0, XF86XK_MonBrightnessDown,            spawn,         {.v = brightness_down}},
+    {0, XF86XK_Launch4,                      spawn,         {.v = profile_set}},
 };
 
 /* button definitions */
@@ -168,33 +165,33 @@ static Key keys[] = {
 static Button buttons[] = {
     /* click                event mask      button          function argument
      */
-    {ClkLtSymbol, 0, Button1, setlayout, {0}},
-    {ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
-    {ClkWinTitle, 0, Button2, zoom, {0}},
-    {ClkStatusText, 0, Button2, spawn, {.v = termcmd}},
-    {ClkClientWin, MODKEY, Button1, movemouse, {0}},
-    {ClkClientWin, MODKEY, Button2, togglefloating, {0}},
-    {ClkClientWin, MODKEY, Button3, resizemouse, {0}},
-    {ClkTagBar, 0, Button1, view, {0}},
-    {ClkTagBar, 0, Button3, toggleview, {0}},
-    {ClkTagBar, MODKEY, Button1, tag, {0}},
-    {ClkTagBar, MODKEY, Button3, toggletag, {0}},
+    {ClkLtSymbol,           0,              Button1,         setlayout,      {0}},
+    {ClkLtSymbol,           0,              Button3,         setlayout,      {.v = &layouts[2]}},
+    {ClkWinTitle,           0,              Button2,         zoom,           {0}},
+    {ClkStatusText,         0,              Button2,         spawn,          {.v = termcmd}},
+    {ClkClientWin,          MODKEY,         Button1,         movemouse,      {0}},
+    {ClkClientWin,          MODKEY,         Button2,         togglefloating, {0}},
+    {ClkClientWin,          MODKEY,         Button3,         resizemouse,    {0}},
+    {ClkTagBar,             0,              Button1,         view,           {0}},
+    {ClkTagBar,             0,              Button3,         toggleview,     {0}},
+    {ClkTagBar,             MODKEY,         Button1,         tag,            {0}},
+    {ClkTagBar,             MODKEY,         Button3,         toggletag,      {0}},
 };
 
 static const char *ipcsockpath = "/tmp/dwm.sock";
 static IPCCommand ipccommands[] = {
-    IPCCOMMAND(view, 1, {ARG_TYPE_UINT}),
-    IPCCOMMAND(toggleview, 1, {ARG_TYPE_UINT}),
-    IPCCOMMAND(tag, 1, {ARG_TYPE_UINT}),
-    IPCCOMMAND(toggletag, 1, {ARG_TYPE_UINT}),
-    IPCCOMMAND(tagmon, 1, {ARG_TYPE_UINT}),
-    IPCCOMMAND(focusmon, 1, {ARG_TYPE_SINT}),
-    IPCCOMMAND(focusstack, 1, {ARG_TYPE_SINT}),
-    IPCCOMMAND(zoom, 1, {ARG_TYPE_NONE}),
-    IPCCOMMAND(incnmaster, 1, {ARG_TYPE_SINT}),
-    IPCCOMMAND(killclient, 1, {ARG_TYPE_SINT}),
-    IPCCOMMAND(togglefloating, 1, {ARG_TYPE_NONE}),
-    IPCCOMMAND(setmfact, 1, {ARG_TYPE_FLOAT}),
-    IPCCOMMAND(setlayoutsafe, 1, {ARG_TYPE_PTR}),
-    IPCCOMMAND(quit, 1, {ARG_TYPE_NONE})
+    IPCCOMMAND(view,            1, {ARG_TYPE_UINT}),
+    IPCCOMMAND(toggleview,      1, {ARG_TYPE_UINT}),
+    IPCCOMMAND(tag,             1, {ARG_TYPE_UINT}),
+    IPCCOMMAND(toggletag,       1, {ARG_TYPE_UINT}),
+    IPCCOMMAND(tagmon,          1, {ARG_TYPE_UINT}),
+    IPCCOMMAND(focusmon,        1, {ARG_TYPE_SINT}),
+    IPCCOMMAND(focusstack,      1, {ARG_TYPE_SINT}),
+    IPCCOMMAND(zoom,            1, {ARG_TYPE_NONE}),
+    IPCCOMMAND(incnmaster,      1, {ARG_TYPE_SINT}),
+    IPCCOMMAND(killclient,      1, {ARG_TYPE_SINT}),
+    IPCCOMMAND(togglefloating,  1, {ARG_TYPE_NONE}),
+    IPCCOMMAND(setmfact,        1, {ARG_TYPE_FLOAT}),
+    IPCCOMMAND(setlayoutsafe,   1, {ARG_TYPE_PTR}),
+    IPCCOMMAND(quit,            1, {ARG_TYPE_NONE})
 };
